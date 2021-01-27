cbc8798557c6e4ea37de4df9663eb786303b2037
JamesPrenticez <Jamesprenticez@gmail.com>
2021-01-28 10:55:34 +1300

    Compare Arrays

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-let arr1 = ["030.csv", "070.csv", "080.csv"];
-let arr2 = ["derp"];
-let arr3 = []
+let arr1 = ["Pre Difined List", "030.csv", "070.csv", "080.csv"];
+let arr2 = [];
 
 // ---------- createArr2  ---------- 
 let a = document.querySelectorAll("a"), i;
 
-function createArr3(){
+function createArr2(){
   for (i = 0; i < a.length; i++){
-    arr3.push(a[i].innerHTML)
+    arr2.push(a[i].innerHTML)
   }
-  console.log(arr3)
-  return arr3
+  console.log(arr1)
+  console.log(arr2)
+  return arr2
  }
 
-console.log(arr3)
 // ---------- Compare Arrays  ---------- 
-let found = arr1.some((r) => arr2.indexOf(r) >= 0);
-
 function compareArrays() {
+  createArr2()
+  let found = arr1.some((r) => arr2.indexOf(r) >= 0);
   if (found === true) {
     console.log("working");
   } else console.log("not working")
