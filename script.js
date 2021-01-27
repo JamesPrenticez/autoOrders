3c2a3cf0f8bba0376d0ed02a90c695c0a4dd5c0a
JamesPrenticez <Jamesprenticez@gmail.com>
2021-01-28 10:44:29 +1300

    Create Arr3

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 let arr1 = ["030.csv", "070.csv", "080.csv"];
-let arr2 = [];
+let arr2 = ["derp"];
+let arr3 = []
 
-let arr3 = document.querySelectorAll("a");
-//console.log(arr3)
+// ---------- createArr2  ---------- 
+let a = document.querySelectorAll("a"), i;
 
+function createArr3(){
+  for (i = 0; i < a.length; i++){
+    arr3.push(a[i].innerHTML)
+  }
+  console.log(arr3)
+  return arr3
+ }
+
+console.log(arr3)
+// ---------- Compare Arrays  ---------- 
 let found = arr1.some((r) => arr2.indexOf(r) >= 0);
 
-function test() {
-  if (found) {
+function compareArrays() {
+  if (found === true) {
     console.log("working");
-  }
+  } else console.log("not working")
 }
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
-}
 
-//console.log(found);
-//console.log(foundtest);
 
-//console.log(arr1, arr2, arr3, arr4)
-// console.clear()
